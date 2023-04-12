193e21ff03be7a576d0935b150fb2eb1f3d735d9
komailhaider010 <komailhaider010@gmail.com>
2023-04-12 09:19:43 +0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
 }
 
-export default App;
+const some = {
+  firstName: 'Harper',
+  lastName: 'Perez'
+};
+
+function GetGreeting({ user }) {
+  if (user) {
+    console.log(user);
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Nigga.</h1>;
+}
+
+export default function App() {
+  return (
+    <>
+      <GetGreeting user={some}/>
+    </>
+  );
+}
